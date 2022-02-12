2f383c559d6cf280857f4324da7efe7f37c061a1
min <wbsldj7645@naver.com>
2022-02-12 16:52:13 +0900

    get, firebase init

---
+++ android/app/src/main/kotlin/com/example/hometending/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hometending
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hometending/src/login/login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FutureBuilder(
+        future: Firebase.initializeApp(),
+        builder: (context, snapshot) {
+          if(snapshot.hasError){
+            return Center(
+              child: Text('firebase load fail'),
+            );
+          }
+          if(snapshot.connectionState == ConnectionState.done) {
+            return LoginPage();
+          }
+          return CircularProgressIndicator();
+        },
+      )
+    );
+  }
+}
