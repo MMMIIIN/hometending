5da18dff04f5847966a7947318ce0a3c4638ccbd
min <wbsldj7645@naver.com>
2022-02-13 16:59:58 +0900

    google, facebook(수정 필요)  auth

--- lib/src/home/page/settings_page.dart
+++ lib/src/home/page/settings_page.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:hometending/src/login/login_page.dart';
 
 class SettingsPage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('SettingsPage'),
+    return SafeArea(
+      child: Column(
+        children: [
+          MaterialButton(
+            elevation: 0,
+            color: Colors.grey,
+            onPressed: () {
+              Get.to(() => LoginPage());
+            },
+            child: Text('로그인'),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -1,13 +1,91 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Login Page'),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              loginContainerForm(0xff20C800, '네이버', false, Colors.white, () {}),
+              loginContainerForm(
+                  0xffFEE500, '카카오톡', false, Colors.black, () {}),
+              loginContainerForm(
+                  0xff3B5998, '페이스북', false, Colors.white, () async {
+              }),
+              loginContainerForm(
+                  0xffffffff, '구글', true, Colors.black, signInWithGoogle),
+              loginContainerForm(0xff000000, '애플', false, Colors.white, () {}),
+              MaterialButton(
+                  color: Colors.orangeAccent,
+                  onPressed: () {
+                    print(FirebaseAuth.instance.currentUser!.email);
+                    print(FirebaseAuth.instance.currentUser!.uid);
+                  }),
+              MaterialButton(
+                  color: Colors.purple,
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                  })
+            ],
+          ),
+        ),
       ),
     );
+  }
+
+  Widget loginContainerForm(int colorValue, String textValue, bool isBorder,
+      Color textColor, Function login) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 5),
+      child: GestureDetector(
+        onTap: () {
+          login();
+        },
+        child: Container(
+          width: double.infinity,
+          height: 60,
+          decoration: BoxDecoration(
+              border: isBorder ? Border.all() : null,
+              color: Color(colorValue),
+              borderRadius: BorderRadius.circular(10)),
+          child: Center(
+            child: Row(
+              children: [
+                SizedBox(width: 20),
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 40),
+                Text(
+                  '$textValue 계정으로 간편로그인',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: textColor,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = (await GoogleSignIn().signIn())!;
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final OAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 }
